7967d90a5b93c996ce328bc7d5579a26954e3dce
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2019-01-02 13:20:07 -0500

    Fix localIdentName in webpack config so that it does not add scoping

--- preact.config.js
+++ preact.config.js
@@ -2,10 +2,16 @@ export default (config, env, helpers) => {
   delete config.entry.polyfills;
   config.output.filename = "[name].js";
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  plugin.options.disable = true;
+  // let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  // plugin.options.disable = true;
 
   if (env.production) {
     config.output.libraryTarget = "umd";
   }
+
+  const { loader: cssLoader } =
+		helpers.getLoadersByName(config, 'css-loader')[0];
+  // loader.options.localIdentName = '[local]';
+  cssLoader.options.localIdentName = '[local]';
+  console.log(111, cssLoader);
 };
