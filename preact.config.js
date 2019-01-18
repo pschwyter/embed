f06d512574f3cd24befff223e4ba82890a2669d7
Samson Kirk-Koffi <samson.kirkkoffi@gmail.com>
2019-01-18 14:15:44 -0500

    Invalidate step and Env variables rename (#5)
    * Invalidate step and Env variables rename.
    Todo: Refactor
    
    * Add cachecontrol for static content
    
    * rm .env from git and add .env.example
    
    * .env
    
    * rm .env again
    
    * NEWLINE
    
    * Refactor

--- preact.config.js
+++ preact.config.js
@@ -1,58 +1,19 @@
-const path = require("path");
-var S3Uploader = require('webpack-s3-uploader')
 require("dotenv").config();
-const { resolve } = path;
+import configSetup from "./parent.config"
 
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
-  config.output.filename = "[name].js";
 
   let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
   plugin.options.disable = true;
 
-  if (env.production) {
-    config.output = {
-      libraryTarget : "umd",
-      filename: 'embed.js',
-    }
-
-    // S3 Upload
-    config.plugins.push(new S3Uploader({
-      include: /.*\.(js)/,
-      exclude: /.*\.(png|json|icon|txt)/,
-      s3Options: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION,
-      },
-      s3UploadOptions: {
-        Bucket: process.env.AWS_BUCKET,
-        CacheControl: "no-cache"
-      },
-    }));
-  }
-
-  // Add loader for TypeScript
-  config.module.loaders.push({
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    loaders: ["ts-loader"]
-  });
+  config = configSetup(config, env.production, false)
 
   // This is to fix the issue where the compiled CSS classnames were given a localIdentName of
   // [local]__[hash], but this did not match the component class names
   const { loader: cssLoader } = helpers.getLoadersByName(config, "css-loader")[0];
   cssLoader.options.localIdentName = "[local]";
 
-  // Directory aliases
-  config.resolve.alias = Object.assign(
-    {},
-    {
-      services: resolve(__dirname, "src/services/"),
-      style: resolve(__dirname, "src/style/")
-    },
-    config.resolve.alias
-  );
-
 
 };
+
