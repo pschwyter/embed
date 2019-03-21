403a084944f0621c566562c259545f3142d55e67
Samson Kirk-Koffi <samson.kirkkoffi@gmail.com>
2019-03-21 15:48:16 -0400

    FND-1002: Quick fix change to CircleCI (#84)
    * Quick fix change to CIrcleCI
    supporting a new branch for quick fix deployment and testing
    
    * typo
    
    * typo

---
+++ preact-hotfix.config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+import configSetup from "./parent.config"
+const childProc = require('child_process');
+
+export default (config, env, helpers) => {
+  delete config.entry.polyfills;
+
+  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  plugin.options.disable = true;
+
+  config = configSetup(config, env.production, helpers, false, "hotfix")
+
+  // This is to fix the issue where the compiled CSS classnames were given a localIdentName of
+  // [local]__[hash], but this did not match the component class names
+  const { loader: cssLoader } = helpers.getLoadersByName(config, "css-loader")[0];
+  cssLoader.options.localIdentName = "[local]";
+
+};
