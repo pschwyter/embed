f3e0994ba42c0b086bebe46ce315a778b38447bd
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2019-03-22 16:10:59 -0400

    Try adding CSP HTML Webpack Plugin

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import configSetup from "./parent.config"
+var CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
@@ -10,9 +11,28 @@ export default (config, env, helpers) => {
   // let { plugin: sehwp } = helpers.getPluginsByName(config, "ScriptExtHtmlWebpackPlugin")[0];
   // sehwp.options.disable = true;
   
-  let { index } = helpers.getPluginsByName(config, 'CommonsChunkPlugin')[0]
-  config.plugins.splice(index, 1)
-  console.log(1111, env.production, config.plugins)
+  // let { index } = helpers.getPluginsByName(config, 'LoaderOptionsPlugin')[0]
+  // index && config.plugins.splice(index, 1)
+  // console.log(1111, env.production, config.plugins)
+  config.plugins.push(
+    new CspHtmlWebpackPlugin({
+      'base-uri': "'self'",
+      'object-src': "'none'",
+      'script-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"],
+      'style-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"]
+    }, {
+      enabled: true,
+      hashingMethod: 'sha256',
+      hashEnabled: {
+        'script-src': true,
+        'style-src': true
+      },
+      nonceEnabled: {
+        'script-src': true,
+        'style-src': true
+      }
+    })
+  )
 
   config = configSetup(config, env.production, helpers, false, null)
 
