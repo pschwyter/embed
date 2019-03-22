5257f3d29b7b2a857d85a1ba78e73eb72814d044
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2019-03-22 16:54:47 -0400

    revert back to 0c20e5e32ebe02460dfa35c543007e9478d87dce

--- preact.config.js
+++ preact.config.js
@@ -1,13 +1,36 @@
 require("dotenv").config();
 import configSetup from "./parent.config"
+var CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  plugin.options.disable = true;
-
+  let { plugin: etp } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  etp.options.disable = true;
+  
   config = configSetup(config, env.production, helpers, false, null)
+
+  config.plugins.push(
+    new CspHtmlWebpackPlugin({
+      'base-uri': "'self'",
+      'object-src': "'none'",
+      'script-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"],
+      'style-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"]
+    }, {
+      enabled: true,
+      hashingMethod: 'sha256',
+      hashEnabled: {
+        'script-src': true,
+        'style-src': true
+      },
+      nonceEnabled: {
+        'script-src': true,
+        'style-src': true
+      }
+    })
+  )
+
+  console.log(1111, env.production, config.plugins)
 
   // This is to fix the issue where the compiled CSS classnames were given a localIdentName of
   // [local]__[hash], but this did not match the component class names
