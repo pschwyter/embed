d386b47cc39726172f32101a9b989c2e236eee20
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2019-03-22 15:25:13 -0400

    Try removing ScriptExtHtmlWebpackPlugin

--- preact.config.js
+++ preact.config.js
@@ -4,8 +4,15 @@ import configSetup from "./parent.config"
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  plugin.options.disable = true;
+  let { plugin: etp } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  etp.options.disable = true;
+  
+  // let { plugin: sehwp } = helpers.getPluginsByName(config, "ScriptExtHtmlWebpackPlugin")[0];
+  // sehwp.options.disable = true;
+  
+  let { index } = helpers.getPluginsByName(config, 'ScriptExtHtmlWebpackPlugin')[0]
+  config.plugins.splice(index, 1)
+  console.log(1111, config.plugins)
 
   config = configSetup(config, env.production, helpers, false, null)
 
