632497a7336ea0f44e663b4b6bd817ff309223e3
Nicholas Haley <nicholashaley@adas-MacBook-Pro.local>
2019-03-25 14:39:24 -0400

    Disable sourceMap on css-loader

--- preact.config.js
+++ preact.config.js
@@ -10,32 +10,9 @@ export default (config, env, helpers) => {
   
   config = configSetup(config, env.production, helpers, false, null)
 
-  config.plugins.push(
-    new CspHtmlWebpackPlugin({
-      'base-uri': "'self'",
-      'object-src': "'none'",
-      'script-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"],
-      'style-src': ["'unsafe-inline'", "'self'", "'unsafe-eval'"]
-    }, {
-      enabled: true,
-      hashingMethod: 'sha256',
-      hashEnabled: {
-        'script-src': true,
-        'style-src': true
-      },
-      nonceEnabled: {
-        'script-src': true,
-        'style-src': true
-      }
-    })
-  )
-
-  console.log(1111, env.production, config.plugins)
-
-  // This is to fix the issue where the compiled CSS classnames were given a localIdentName of
   // [local]__[hash], but this did not match the component class names
   const { loader: cssLoader } = helpers.getLoadersByName(config, "css-loader")[0];
   cssLoader.options.localIdentName = "[local]";
-
+  cssLoader.options.sourceMap = false;
 };
 
