0ef3274a3c620319bb11b945c7a6fd88f8e6398a
Nicholas Haley <nicholashaley@adas-MacBook-Pro.local>
2019-03-25 14:51:00 -0400

    disable sourcemaps for postcss-loader

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 import configSetup from "./parent.config"
-var CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
@@ -13,6 +12,9 @@ export default (config, env, helpers) => {
   // [local]__[hash], but this did not match the component class names
   const { loader: cssLoader } = helpers.getLoadersByName(config, "css-loader")[0];
   cssLoader.options.localIdentName = "[local]";
-  cssLoader.options.sourceMap = false;
+  // cssLoader.options.sourceMap = false;
+
+  const { loader: postCSSLoader } = helpers.getLoadersByName(config, "postcss-loader")[0];
+  // postCSSLoader.options.sourceMap = false;
 };
 
