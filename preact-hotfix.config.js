570e1e6e7bc78037b3a30cb734b14cba84f88f5d
Nicholas Haley <nicholashaley@adas-MacBook-Pro.local>
2019-03-25 16:51:52 -0400

    Cleanup

--- preact-hotfix.config.js
+++ preact-hotfix.config.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 import configSetup from "./parent.config"
-const childProc = require('child_process');
 
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
@@ -9,19 +8,6 @@ export default (config, env, helpers) => {
   plugin.options.disable = true;
 
   config = configSetup(config, env.production, helpers, false, "hotfix")
-
-  let { plugin: htmlPlugin } = helpers.getPluginsByName(config, "HtmlWebpackPlugin")[0];
-  htmlPlugin.options.minify = {
-    collapseWhitespace: false,
-    removeComments: false,
-    removeRedundantAttributes: false,
-    removeScriptTypeAttributes: false,
-    removeStyleLinkTypeAttributes: false,
-    useShortDoctype: false
-  };
-  htmlPlugin.options.cache = false;
-  htmlPlugin.options.hash = true;
-  htmlPlugin.options.inject = false;
 
   // This is to fix the issue where the compiled CSS classnames were given a localIdentName of
   // [local]__[hash], but this did not match the component class names
