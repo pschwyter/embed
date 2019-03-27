3061d72d103fc1dfa44945e68ad6e85464eca906
seva <seva@ada.support>
2019-03-27 14:08:01 -0400

    Try StyleExtHtmlWebpackPlugin: try dev?

--- preact-hotfix.config.js
+++ preact-hotfix.config.js
@@ -14,38 +14,6 @@ export default (config, env, helpers) => {
   const { loader: cssLoader } = helpers.getLoadersByName(config, "css-loader")[0];
   cssLoader.options.localIdentName = "[local]";
 
-  const cssLoaderRules = helpers.getRules(config).filter(rule => {
-    return String(rule.rule.test) === String(/\.(css|less|s[ac]ss|styl)$/)
-  });
-
-  cssLoaderRules.forEach(rule => {
-    rule.loader = null;
-    rule.use = [
-      {
-        loader: "style-loader",
-        options: {
-          insertAt: 'top', //Insert style tag at top of <head>
-          singleton: true, //this is for wrap all your style in just one style tag
-        }
-      },
-      {
-        loader: 'css-loader',
-        options: {
-          modules: true,
-          localIdentName: '[local]__[hash:base64:5]',
-          importLoaders: 1,
-          sourceMap: true
-        }
-      }, {
-        loader: 'postcss-loader',
-        options: {
-          ident: 'postcss',
-          sourceMap: true
-        }
-      }
-    ]
-  })
-
   // const { loader: styleLoader } = helpers.getLoadersByName(config, "style-loader")[0];
   // cssLoader.options.insertAt = "top";
   // cssLoader.options.singleton = true;
