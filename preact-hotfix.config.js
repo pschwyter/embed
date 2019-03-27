a3898e50f42cc58f31e0108d6a00eb2962abdf8a
seva <seva@ada.support>
2019-03-27 17:37:01 -0400

    Try StyleExtHtmlWebpackPlugin: wait what?

--- preact-hotfix.config.js
+++ preact-hotfix.config.js
@@ -5,15 +5,19 @@ const childProc = require('child_process');
 export default (config, env, helpers) => {
   delete config.entry.polyfills;
 
-  let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
-  plugin.options.disable = true;
+  // let { plugin } = helpers.getPluginsByName(config, "ExtractTextPlugin")[0];
+  // plugin.options.disable = true;
 
   config = configSetup(config, env.production, helpers, false, "hotfix")
 
-  // This is to fix the issue where the compiled CSS classnames were given a localIdentName of
   // [local]__[hash], but this did not match the component class names
   const { loader: cssLoader } = helpers.getLoadersByName(config, "css-loader")[0];
   cssLoader.options.localIdentName = "[local]";
+  cssLoader.options.sourceMap = false;
+
+  const { loader: postCSSLoader } = helpers.getLoadersByName(config, "postcss-loader")[0];
+  postCSSLoader.options.sourceMap = false;
+
 
   const cssLoaders = helpers.getLoaders(config).filter(loader => {
     return loader.rule.test.toString() === `/\\.(css|less|s[ac]ss|styl)$/`.toString()
@@ -22,22 +26,7 @@ export default (config, env, helpers) => {
   cssLoaders.forEach(loader => {
     loader.rule.loader = [
       "style-loader",
-      {
-        loader: "css-loader",
-        options: {
-          modules: true,
-          localIdentName: "[local]__[hash:base64:5]",
-          importLoaders: 1,
-          sourceMap: false
-        }
-      }, {
-        loader: "postcss-loader",
-        options: {
-          ident: "postcss",
-          sourceMap: true,
-          plugins: () => [require("autoprefixer")]
-        }
-      }
+      "css-loader"
     ];
   });
 };
